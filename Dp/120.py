1c2230b0ea9832dbbcfff72d0768c23386e1d115
kt4ngw <kt4ngw@163.com>
2022-10-17 16:14:52 +0800

    update 120.py

--- Dp/120.py
+++ Dp/120.py
@@ -1,11 +1,15 @@
 class Solution:
     def minimumTotal(self, triangle) -> int:
-        output = 0
-        for i in range(len(triangle)):
-            output = output + min(triangle[i])
-        return output
-
-
-c = Solution()
-print(c.minimumTotal([[-1],[2,3],[1,-1,-3]]))
-print(-1 + 2 - 3)
+        result = triangle
+        for i in range(1, len(triangle)):
+            for j in range(len(triangle[i])):
+                if j == 0:
+                    result[i][j] = result[i][j] + result[i - 1][j]
+                elif j == len(triangle[i]) - 1:
+                    result[i][j] = result[i][j] + result[i - 1][j - 1]
+                else:
+                    if result[i][j] + result[i - 1][j - 1] < result[i][j] + result[i - 1][j]:
+                        result[i][j] = result[i][j] + result[i - 1][j - 1]
+                    else:
+                        result[i][j] = result[i][j] + result[i - 1][j]
+        return min(result[-1])
