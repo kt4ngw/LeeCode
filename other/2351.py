1986b3c5878d27b1580da4534bd92ffeaced2027
kt4ngw <kt4ngw@163.com>
2023-01-09 10:58:38 +0800

    upload 2351.py

--- other/2351.py
+++ other/2351.py
@@ -1,11 +1,15 @@
 class Solution:
-    def countAsterisks(self, s: str) -> int:
-        count = 0  # 计数 *
-        count_ = 0  # 计数 “|”
-        for i in s:
-            if i == '|':
-                count_ += 1
-            elif (count_ % 2 == 0 and i == '*'):
-                count += 1
-        return count
-        # 什么情况下去计数 count ? 做一个flag标记
+    def repeatedCharacter(self, s: str) -> str:
+        # 一个 a - z 的数组 两列
+        # 第一列 记录 出现次数 第二列记录出现第二次出现的位置 如果在第二列出现了，就可以确定是这个，直接break，那么第三次出现怎么办？第三次直接 已经break了
+
+        arr = [[0] * 2 for i in range(26)]
+        for i in range(len(s)):
+            if arr[ord(s[i]) - 97][0] == 0:
+                arr[ord(s[i]) - 97][0] = 1  # 出现了第一个
+            else:  # 第一个已经出现
+                arr[ord(s[i]) - 97][1] = i + 1
+                break
+        for i in range(26):
+            if(arr[i][0] == 1 and arr[i][1]>0):
+                return chr(i + 97)
