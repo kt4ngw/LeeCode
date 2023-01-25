62f63e96b0fb0db4e8eb149605295d2e22202302
kt4ngw <kt4ngw@163.com>
2023-01-25 20:59:20 +0800

    upload 47.py

---
+++ 47.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(nums, temp):
+            if 0 == len(nums):
+                result.append(temp)
+            else:
+                for i in range(len(nums)):
+                    # 从 i 之前能找到
+                    flag = 1  # 没有相同的
+                    piont = i
+                    while (piont >= 1):
+                        if (nums[i] == nums[piont - 1]):
+                            flag = 0
+                            break
+                        piont -= 1
+                    if flag == 1:
+                        backtrack(nums[:i] + nums[i + 1:], temp + [nums[i]])
+
+        backtrack(nums, [])
+        return result
