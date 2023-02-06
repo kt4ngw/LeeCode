2c27be7e71565339135a62540a0c06b4ad50a744
kt4ngw <kt4ngw@163.com>
2023-02-06 09:23:53 +0800

    upload 2331.py

---
+++ binaryTree/2331.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        def inOrder(root):
+            if root == None:
+                return
+            left = inOrder(root.left)
+            right = inOrder(root.right)
+            if root.val == 0:
+                return False
+            elif root.val == 1:
+                return True
+            elif root.val == 2:
+                return left or right
+            elif root.val == 3:
+                return left and right
+        return inOrder(root)
